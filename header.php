aec016c270e56a509a98b569526cf776e38cbb9b
Vasily Myazin <vm@konspiredesign.com>
2017-01-19 23:35:31 -0500

    Improve header, add favicons

--- header.php
+++ header.php
@@ -15,6 +15,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width">
 
+    
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri() ?>/img/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri() ?>/img/favicons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri() ?>/img/favicons/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="<?php echo get_template_directory_uri() ?>/img/favicons/manifest.json">
+    <link rel="mask-icon" href="<?php echo get_template_directory_uri() ?>/img/favicons/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri() ?>/img/favicons/favicon.ico">
+    <meta name="msapplication-config" content="<?php echo get_template_directory_uri() ?>/img/favicons/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
