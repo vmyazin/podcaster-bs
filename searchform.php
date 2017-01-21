f4aff85b08d03b9885c6cc823adb8267b02d9c58
Vasily Myazin <vm@konspiredesign.com>
2017-01-21 00:18:55 -0500

    Update search placeholder

--- searchform.php
+++ searchform.php
@@ -8,7 +8,7 @@
 <form role="search" method="get" class="search-form form" action="<?php echo esc_url(home_url('/')); ?>">
 	<label for="form-search-input" class="sr-only"><?php _ex('Search for', 'label', 'bootstrap-basic'); ?></label>
 	<div class="input-group">
-		<input type="search" id="form-search-input" class="form-control" placeholder="<?php echo esc_attr_x('Search &hellip;', 'placeholder', 'bootstrap-basic'); ?>" value="<?php echo esc_attr(get_search_query()); ?>" name="s" title="<?php echo esc_attr_x('Search for:', 'label', 'bootstrap-basic'); ?>">
+		<input type="search" id="form-search-input" class="form-control" placeholder="<?php echo esc_attr_x('Look for&hellip;', 'placeholder', 'bootstrap-basic'); ?>" value="<?php echo esc_attr(get_search_query()); ?>" name="s" title="<?php echo esc_attr_x('Search for:', 'label', 'bootstrap-basic'); ?>">
 		<span class="input-group-btn">
 			<button type="submit" class="btn btn-default"><?php esc_html_e('Search', 'bootstrap-basic'); ?></button>
 		</span>
