6f9740382da2f8f2a9428c4159859ece52cb1752
Vasily Myazin <vasily@amastermind.com>
2018-04-01 20:59:44 -0500

    Update sidebar

--- sidebar-right.php
+++ sidebar-right.php
@@ -6,13 +6,13 @@
             <h1 class="widget-title">Subscribe</h1>
             <ul class="subscribe-links">
               <li>
-                <a href="https://overcast.fm">
+                <a href="https://overcast.fm/itunes1200334914/everyday-journey-podcast">
                   <img src="<?php echo get_template_directory_uri() ?>/img/icons/icon-overcast.svg" />
                   <p>Overcast</p>
                 </a>
               </li>
               <li>
-                <a href="http://www.itunes.com">
+                <a href="https://itunes.apple.com/us/podcast/everyday-journey-podcast/id1200334914?mt=2">
                   <img src="<?php echo get_template_directory_uri() ?>/img/icons/icon-itunes.svg" />
                   <p>iTunes</p>
                 </a>
