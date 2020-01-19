d0eb0860c461a0907fe2ee3bc5ac9dcb39de09ce
Vasily Myazin <vm@konspiredesign.com>
2020-01-18 19:33:25 -0500

    Update latest episodes section layout

--- latest-episodes.php
+++ latest-episodes.php
@@ -11,11 +11,16 @@
     return get_post_meta($Id, $field_name, true);
   }
 ?>
-    
+
 <?php
   $recent_posts = wp_get_recent_posts($args, ARRAY_A);
     foreach( $recent_posts as $recent ) {
-      echo '<div class="col-md-2 col-sm-3 col-xs-4 mini-episode"><div class="latest-ep-avatar"><img src="' .get_custom_field('episode_avatar_img', $recent["ID"]).'"></div><span class="badge">' .get_custom_field('episode_number', $recent["ID"]).'</span><a href="' . get_permalink($recent["ID"]) . '">' .   $recent["post_title"].'</a> </div> ';
+      if (has_post_thumbnail( $recent["ID"] ) ) {
+        $image = wp_get_attachment_image_src( get_post_thumbnail_id( $recent["ID"] ), 'single-post-thumbnail' );
+      } else {
+        $image = '';
+      }
+      echo '<div class="col-md-2 col-sm-3 col-xs-4 mini-episode"><div class="latest-ep-avatar"><img src="' . $image[0] .'"></div><span class="badge">#' . get_custom_field('episode_number', $recent["ID"]).'</span><a href="' . get_permalink($recent["ID"]) . '">' . $recent["post_title"].'</a> </div> ';
     }
   wp_reset_query();
 ?>   
